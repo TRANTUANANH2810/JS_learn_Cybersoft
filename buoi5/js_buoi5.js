58873ddd318c136e01b7202c73c6610346d862a3
Tuananh2810 <trantuananh.28102002@gmail.com>
2024-05-06 14:45:40 +0700

    Buoi5/JS

---
+++ buoi5/js_buoi5.js
@@ -0,0 +1,17 @@
+/**
+ * vong lap for
+ */
+
+function Xuat10Cau() {
+    for (var i = 0; i < 10; i++) {
+        document.write("xin chao Tuan Anh " + i + "<br/>");
+    }
+}
+
+function inSoLe() {
+    for (var i = 0; i < 100; i++) {
+        if (i % 2 != 0) {
+            document.write(" So le la ::: " + i + "<br/>");
+        }
+    }
+}
