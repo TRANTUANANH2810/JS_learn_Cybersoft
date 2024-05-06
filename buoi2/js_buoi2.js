f928f81667f18d857db2949f942dc5a724be1dbe
Tuananh2810 <trantuananh.28102002@gmail.com>
2024-05-06 13:26:08 +0700

    buoi2

---
+++ buoi2/js_buoi2.js
@@ -0,0 +1,19 @@
+// ! Hàm
+
+/**
+ * function Ten_ham(thamso1, thamso2) {
+ * ...
+ * }
+ *
+ * function Ten_ham(thamso1, thamso2) {
+ * return (value);
+ * }
+ */
+//  vd
+function sum(x, y) {
+    tong = x + y;
+    return tong;
+}
+
+var out = sum(10, 20);
+console.log("Tong la:::" + " " + out);
