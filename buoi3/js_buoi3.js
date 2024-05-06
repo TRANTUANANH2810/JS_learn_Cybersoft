ae98635d9f4828dbbcb7b57957223e8154b36211
Tuananh2810 <trantuananh.28102002@gmail.com>
2024-05-06 13:43:24 +0700

    buoi3/JS

---
+++ buoi3/js_buoi3.js
@@ -0,0 +1,14 @@
+/**
+ *  if/else
+ *
+ */
+
+function kienTraNhap(giatri) {
+    if (giatri == 8) {
+        alert("so 8");
+    } else if (giatri == 9) {
+        alert("so 9");
+    } else {
+        alert(" khong phai 8 va 9");
+    }
+}
